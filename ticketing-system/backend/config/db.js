15d2875f0aedf6773acd1bb9a8e65120b0095d5f
Soumay <soumayagarwal1707@gmail.com>
2026-07-14 13:56:10 +0530

    Corrected log statement in backend server

--- ticketing-system/backend/config/db.js
+++ ticketing-system/backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
     try{
         await mongoose.connect(process.env.MONGO_URI);
-        console.log("Connected to local MongoDB successfully.");
+        console.log("Connected to MongoDB Atlas successfully.");
     }
     catch(err){
         console.log("MongoDB connection failed:", err);
